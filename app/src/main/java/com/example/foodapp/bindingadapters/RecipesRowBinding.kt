233197e4e6768ee123a374dc25d8b105e6516c53
Rafic Houssari <rafic.houssari@cmeoffshore.com>
2021-11-16 21:30:25 +0200

    Design Overview Fragment

--- app/src/main/java/com/example/foodapp/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foodapp/bindingadapters/RecipesRowBinding.kt
@@ -12,6 +12,7 @@ import coil.load
 import com.example.foodapp.R
 import com.example.foodapp.models.Result
 import com.example.foodapp.ui.fragments.recipes.RecipesFragmentDirections
+import org.jsoup.Jsoup
 
 class RecipesRowBinding {
 
@@ -75,6 +76,15 @@ class RecipesRowBinding {
                         )
                     }
                 }
+            }
+        }
+
+        @BindingAdapter("parseHtml")
+        @JvmStatic
+        fun parseHtml(textView: TextView, description: String?) {
+            if(description != null) {
+                val desc = Jsoup.parse(description).text()
+                textView.text = desc
             }
         }
 
