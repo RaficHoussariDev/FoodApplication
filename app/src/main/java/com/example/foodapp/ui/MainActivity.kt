9248876473d0bc006c7ac0d379b792eb5d7d9b50
RaficHoussariDev <rafic.houssari@gmail.com>
2021-12-21 21:00:03 +0200

    Splash screen

--- app/src/main/java/com/example/foodapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/ui/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FoodApp)
         setContentView(R.layout.activity_main)
 
         val navController = Navigation.findNavController(this, R.id.navHostFragment)
