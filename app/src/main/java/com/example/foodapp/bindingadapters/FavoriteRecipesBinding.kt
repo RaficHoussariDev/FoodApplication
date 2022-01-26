c9a466f5b01875c14920f631a1833a3b840d8c38
RaficHoussariDev <rafic.houssari@gmail.com>
2022-01-26 21:25:29 +0200

    Code cleanup in favorite recipes binding

--- app/src/main/java/com/example/foodapp/bindingadapters/FavoriteRecipesBinding.kt
+++ app/src/main/java/com/example/foodapp/bindingadapters/FavoriteRecipesBinding.kt
@@ -1,8 +1,6 @@
 package com.example.foodapp.bindingadapters
 
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foodapp.adapters.FavoriteRecipesAdapter
@@ -21,18 +19,16 @@ class FavoriteRecipesBinding {
         ) {
             if(favoritesEntity.isNullOrEmpty()) {
                 when(view) {
-                    is ImageView -> view.visibility = View.VISIBLE
-                    is TextView -> view.visibility = View.VISIBLE
                     is RecyclerView -> view.visibility = View.INVISIBLE
+                    else -> view.visibility = View.VISIBLE
                 }
             } else {
                 when(view) {
-                    is ImageView -> view.visibility = View.INVISIBLE
-                    is TextView -> view.visibility = View.INVISIBLE
                     is RecyclerView -> {
                         view.visibility = View.VISIBLE
                         mAdapter?.setData(favoritesEntity)
                     }
+                    else -> view.visibility = View.INVISIBLE
                 }
             }
         }
